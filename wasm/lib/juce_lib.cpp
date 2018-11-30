cc0a695b308316351fffb05ec8cf907a1e35bdeb
jariseon <jari.kleimola@outlook.com>
2018-11-30 12:16:53 +0200

    recompiled with emscripten 1.38.20

--- wasm/lib/juce_lib.cpp
+++ wasm/lib/juce_lib.cpp
@@ -1,16 +1,16 @@
-#include <AppConfig.h>
+#include "AppConfig.h"
 
 // -- core
 #include <unistd.h>
 #include <signal.h>
-#include "../../../JuceLibraryCode/modules/juce_core/juce_core.cpp"
-#include "../../../JuceLibraryCode/modules/juce_core/native/juce_audioworklet.cpp"
+#include "../../modules/juce_core/juce_core.cpp"
+#include "../../modules/juce_core/native/juce_audioworklet.cpp"
 
 // -- events
-#include "../../../JuceLibraryCode/modules/juce_events/juce_events.cpp"
-#include "../../../JuceLibraryCode/modules/juce_events/native/juce_audioworklet.cpp"
+#include "../../modules/juce_events/juce_events.cpp"
+#include "../../modules/juce_events/native/juce_audioworklet.cpp"
 
 // -- audio
-#include "../../../JuceLibraryCode/modules/juce_audio_basics/juce_audio_basics.cpp"
-#include "../../../JuceLibraryCode/modules/juce_audio_processors/juce_audio_processors.cpp"
-#include "../../../JuceLibraryCode/modules/juce_audio_plugin_client/utility/juce_PluginUtilities.cpp"
+#include "../../modules/juce_audio_basics/juce_audio_basics.cpp"
+#include "../../modules/juce_audio_processors/juce_audio_processors.cpp"
+#include "../../modules/juce_audio_plugin_client/utility/juce_PluginUtilities.cpp"
